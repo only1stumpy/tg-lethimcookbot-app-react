3d2caf5283762405aa50178a6886e8fe4f8003e7
only1stumpy <rei228322@gmail.com>
2024-05-28 20:39:20 +0300

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -7,7 +7,7 @@ const Form = () => {
   const [age, setAge] = useState("");
   const [desc, setDesc] = useState('');
   const { user, tg } = useTelegram();
-  const username = user?.username;
+  const username = tg.initDataUnsafe?.user?.username;
 
   const onSendData = useCallback(async () => {
     const data = {
