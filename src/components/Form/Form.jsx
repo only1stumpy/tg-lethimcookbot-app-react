46c278ffc8485a7062ed6e73a7bed40d94e209bf
only1stumpy <rei228322@gmail.com>
2024-05-28 19:02:15 +0300

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,93 +1,102 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './Form.css';
-import {useTelegram} from "../../hooks/useTelegram";
+import { useTelegram } from "../../hooks/useTelegram";
 
 const Form = () => {
-    const [nickname, setNickname] = useState('');
-    const [age, setAge] = useState("");
-    const [desc, setDesc] = useState('');
-    const [photo, setPhoto] = useState('');
-    const {tg} = useTelegram();
+  const [nickname, setNickname] = useState('');
+  const [age, setAge] = useState("");
+  const [desc, setDesc] = useState('');
+  const [photo, setPhoto] = useState(null);
+  const { user, tg } = useTelegram();
+  const username = user?.username;
 
-    const onSendData = useCallback(() => {
-        const data = {
-            nickname,
-            age,
-            desc,
-            photo
-        }
-        tg.sendData(JSON.stringify(data));
-    }, [nickname, age, desc, photo])
+  const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
 
-    useEffect(() => {
-        tg.onEvent('mainButtonClicked', onSendData)
-        return () => {
-            tg.offEvent('mainButtonClicked', onSendData)
-        }
-    }, [onSendData])
+  const onSendData = useCallback(async () => {
+    const photoBase64 = photo ? await toBase64(photo) : '';
+    const data = {
+      username,
+      nickname,
+      age,
+      desc,
+      photo: photoBase64
+    };
+    tg.sendData(JSON.stringify(data));
+  }, [username, nickname, age, desc, photo]);
 
-    useEffect(() => {
-        tg.MainButton.setParams({
-            text: 'Отправить данные'
-        })
-    }, [])
+  useEffect(() => {
+    tg.onEvent('mainButtonClicked', onSendData);
+    return () => {
+      tg.offEvent('mainButtonClicked', onSendData);
+    };
+  }, [onSendData]);
 
-    useEffect(() => {
-        if(!nickname || !age || !desc) {
-            tg.MainButton.hide();
-        } else {
-            tg.MainButton.show();
-        }
-    }, [nickname, age, desc])
+  useEffect(() => {
+    tg.MainButton.setParams({
+      text: 'Отправить данные'
+    });
+  }, [tg.MainButton]);
 
-    const onChangeNickname = (e) => {
-        setNickname(e.target.value)
+  useEffect(() => {
+    if (!nickname || !age || !desc) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
     }
+  }, [nickname, age, desc, tg.MainButton]);
 
-    const onChangeAge = (e) => {
-        setAge(e.target.value)
-    }
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+  };
 
-    const onChangeDesc = (e) => {
-        setDesc(e.target.value)
-    }
-    const onChangePhoto  =  (e)  =>  {
-        setPhoto(e.target.files[0])
-    }
+  const onChangeAge = (e) => {
+    setAge(e.target.value);
+  };
 
-    return (
-        <div className={"form"}>
-            <h3>Введите ваши данные</h3>
-            <input
-                className={'input'}
-                type="text"
-                placeholder={'Игровой ник'}
-                value={nickname}
-                onChange={onChangeNickname}
-            />
-            <input
-                className={'input'}
-                type="text"
-                placeholder={'Ваш возраст'}
-                value={age}
-                onChange={onChangeAge}
-            />
-            <input
-                className={'input'}
-                type="text"
-                placeholder={'Почему вы решили вступить к нам?'}
-                value={desc}
-                onChange={onChangeDesc}
-            />
-            <input
-                className={'input'}
-                type="file"
-                placeholder={'Скриншот вашего аккаунта'}
-                value={photo}
-                onChange={onChangePhoto}
-            />
-        </div>
-    );
+  const onChangeDesc = (e) => {
+    setDesc(e.target.value);
+  };
+
+  const onChangePhoto = (e) => {
+    setPhoto(e.target.files[0]);
+  };
+
+  return (
+    <div className={"form"}>
+      <h3>Введите ваши данные</h3>
+      <input
+        className={'input'}
+        type="text"
+        placeholder={'Игровой ник'}
+        value={nickname}
+        onChange={onChangeNickname}
+      />
+      <input
+        className={'input'}
+        type="text"
+        placeholder={'Ваш возраст'}
+        value={age}
+        onChange={onChangeAge}
+      />
+      <input
+        className={'input'}
+        type="text"
+        placeholder={'Почему вы решили вступить к нам?'}
+        value={desc}
+        onChange={onChangeDesc}
+      />
+      <input
+        className={'input'}
+        type="file"
+        onChange={onChangePhoto}
+      />
+    </div>
+  );
 };
 
 export default Form;
