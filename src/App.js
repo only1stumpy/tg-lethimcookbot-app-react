cfa1f7f6f7ec6f99288a795567bee4c897923995
only1stumpy <rei228322@gmail.com>
2024-05-28 18:44:54 +0300

    init

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ function App() {
     }, [])
 
     return (
-        <div className="App">
-            <Header />
+        <div>
             <Routes>
                 <Route index element={<Rules />}/>
                 <Route path={'form'} element={<Form />}/>
